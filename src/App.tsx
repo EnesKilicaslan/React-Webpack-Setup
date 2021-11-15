3efc302b0ae79593372da64c4f8488a9b9edb0a7
EnesKilicaslan <eeneskilicaslan@gmail.com>
2021-11-15 14:00:15 -0800

    Add style, png/svg asset configs to webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import "./styles.css";
+import IMAGE from "./react.png";
+import SVG from "./square.svg";
 
 export const App = () => {
-  return <h1> React Project Setup</h1>;
+  return (
+    <>
+      <h1> React Project Setup</h1>;
+      <img src={IMAGE} height={300} width={300} />
+      <img src={SVG} alt="svg" />
+    </>
+  );
 };
