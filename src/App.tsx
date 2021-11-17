b772a19a0d308c1d31aad0f3096697d20d10e861
EnesKilicaslan <eeneskilicaslan@gmail.com>
2021-11-17 01:06:34 -0800

    add prod and dev webpack configurations

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import SVG from "./square.svg";
 export const App = () => {
   return (
     <>
-      <h1> React Project Setup</h1>;
+      <h1>
+        React Project Setup {process.env.NODE_ENV} {process.env.porting}
+      </h1>
+      ;
       <img src={IMAGE} height={300} width={300} />
       <img src={SVG} alt="svg" />
     </>
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,40 +1,10 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "bundle.js",
-  },
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
-    }),
-  ],
+module.exports = (envVars) => {
+  const { env } = envVars;
+  const envConfig = require(`./webpack.${env}.js`);
+  const config = merge(commonConfig, envConfig);
+
+  return config;
 };
