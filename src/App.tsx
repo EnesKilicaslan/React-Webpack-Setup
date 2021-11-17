6be256616e791ba1d7e50fbfe9f93d9b914be10c
EnesKilicaslan <eeneskilicaslan@gmail.com>
2021-11-17 01:55:52 -0800

    add react webpack refresh for single component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import "./styles.css";
 import IMAGE from "./react.png";
 import SVG from "./square.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
     <>
       <h1>
-        React Project Setup {process.env.NODE_ENV} {process.env.porting}
+        React dProject Setup {process.env.NODE_ENV} {process.env.porting}
       </h1>
-      ;
+
       <img src={IMAGE} height={300} width={300} />
       <img src={SVG} alt="svg" />
+
+      <ClickCounter />
     </>
   );
 };
---
+++ src/ClickCounter.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export const ClickCounter = () => {
+  const [clickCount, setClickCount] = useState(0);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setClickCount((c) => c + 1);
+        }}
+      >
+        Click {clickCount} -
+      </button>
+    </div>
+  );
+};
